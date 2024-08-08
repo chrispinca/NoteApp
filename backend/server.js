0f2aac713ad185c7235fde931a2946cc29819bb3
cpinca <cpinca@sfu.ca>
2024-08-08 13:47:28 -0700

    get specific note + update a note routes implemented

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 import {PORT, mongoDBURL } from "./config.js";
 import Note from "./models/noteModel.js";
 
+//setup express
 const app = express();
 
 app.use(express.json());
 
+//route for homepage
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(234).send("Welcome to note app");
@@ -43,14 +45,53 @@ app.get('/notes', async (req, res) => {
     try {
         const notes = await Note.find({});
 
-        return res.status(200).json(notes);
+        return res.status(200).json({
+            count: notes.length,
+            data: notes
+    });
     } catch (error) {
         console.log(error.message);
         res.status(500).send({message: error.message});
     }
 });
 
+//route to get a specific note
+app.get('/notes/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const note = await Note.findById(id);
 
+        return res.status(200).json(note);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
+//Route to update a specific note
+app.put('/notes/:id', async (req, res) => {
+    try {
+        if (!req.body.title ||
+            !req.body.note
+        ) {
+            return res.status(400).send({
+                message: 'Send all required fields: title, note',
+            });
+        }
+        const {id} = req.params;
+        const result = await Note.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            return res.status(404).json({message: 'Note not found'});
+        }
+        return res.status(200).send({message: 'Note updated successfully!'});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+});
+
+//mongoDB database connection
 mongoose
     .connect(mongoDBURL)
     .then(() => {
